c46422425d0a459b7a5294c1dfcb7574aa5182fb
nafirar <danisfira.nr@gmail.com>
2025-06-21 22:45:29 +0700

    docs : menambahkan fungsi reverseArray menggunakan built-in function .reverse()

--- soal2.js
+++ soal2.js
@@ -291,6 +291,7 @@ console.log(getDayName(8)); // Output: "Nomor hari tidak valid"
 
 function reverseArray(arr) {
     // code disini yaa
+    return arr.reverse()
 }
 
 console.log(reverseArray([1, 2, 3, 4, 5])); // Output: [5, 4, 3, 2, 1]
